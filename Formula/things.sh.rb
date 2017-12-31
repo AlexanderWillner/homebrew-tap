394767abda0e6767cfaa1d30906272fb6c39e8ee
Alexander Willner <alex@willner.ws>
2017-12-31 14:37:16 +0100

    v1.3

--- Formula/things.sh.rb
+++ Formula/things.sh.rb
@@ -1,8 +1,8 @@
 class ThingsSh < Formula
   desc "Simple read-only comand-line interface to your Things 3 database"
   homepage "https://github.com/AlexanderWillner/things.sh"
-  url "https://github.com/AlexanderWillner/things.sh/archive/1.2.tar.gz"
-  sha256 "3effb17c48a86b3e501b90d4205683511027e76ab1e5400fe6aee10248ca089f"
+  url "https://github.com/AlexanderWillner/things.sh/archive/1.3.tar.gz"
+  sha256 "65908f00a0dce0adbc248ae19521886d3c3bec7c4c5392dd3f3d33ff6c94e0ce"
 
   def install
     bin.install "things.sh"

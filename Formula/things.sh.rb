1dd768382049cc24b03af62520ba2207064a9b13
Alexander Willner <alex@willner.ws>
2018-01-04 19:25:53 +0100

    typo

--- Formula/things.sh.rb
+++ Formula/things.sh.rb
@@ -4,6 +4,12 @@ class ThingsSh < Formula
   url "https://github.com/AlexanderWillner/things.sh/archive/2.0.tar.gz"
   sha256 "3fba877cc00915b55990e54d81526e6d6d9bf01055416486811e8dac3069b851"
 
+  bottle do
+    root_url "https://github.com/AlexanderWillner/things.sh/releases/download/2.0/"
+    cellar :any_skip_relocation
+    sha256 "5327d6b9c93e1959817a6b8a0b309dfdce2359949d310c60b56067207c720709" => :high_sierra
+  end
+
   def install
     bin.install "things.sh"
     bin.install "plugins"
@@ -13,10 +19,4 @@ class ThingsSh < Formula
   test do
     system "true" # nothing really to test here
   end
-
-  bottle do
-    root_url "https://github.com/AlexanderWillner/things.sh/releases/download/2.0/"
-    cellar :any_skip_relocation
-    sha256 "5327d6b9c93e1959817a6b8a0b309dfdce2359949d310c60b56067207c720709" => :high_sierra
-  ends
 end

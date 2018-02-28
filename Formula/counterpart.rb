5a72376ee9c98a4d12b9ff67f79f6e9ae5d5ea2d
Alexander Willner <alex@willner.ws>
2018-02-28 13:33:24 +0100

    fixed shasum

--- Formula/counterpart.rb
+++ Formula/counterpart.rb
@@ -4,7 +4,7 @@ class Counterpart < Formula
   desc "A wrapper script for rsync 3.0+ that is capable of producing a bootable clone of a live Mac OS X system."
   homepage 'http://jedda.me/counterpart'
   url 'https://github.com/AlexanderWillner/Counterpart/archive/1.3.tar.gz'
-  sha256 '6bd2bb7b0191bee687f18023209e95b684bae452'
+  sha256 '94be38d9e758b6aa043964e8de0a1a2f9b908fe38887718be0c10e280c5cd2ef'
 
   #depends_on 'https://raw.github.com/Homebrew/homebrew-dupes/109dca908c6499116e07483d7afe8a1c3ef63ad6/rsync.rb'
 

5cbeaa2d219cbdd1a7bd566091f9d93c147c7d25
Alexander Willner <alex@willner.ws>
2020-07-27 09:39:22 +0200

    fixing tap

--- Formula/md2src.rb
+++ Formula/md2src.rb
@@ -1,9 +1,9 @@
-class md2src < Formula
+class Md2src < Formula
   version "1.0.0"
   desc "Markdown to source. Extracts code blocks marked with triple backticks into files."
   homepage "https://github.com/AlexanderWillner/md2src"
   url "https://github.com/AlexanderWillner/md2src/releases/download/v#{version}/md2src-#{version}-mac.tar.gz"
-  sha256 "745d54334e1de3f839bb0b06c30e9445ace0ab663ac629f50841c637d619ca39b"
+  sha256 "745d54334e1de3f839bb0b06c30e9445ace0ab663ac629f50841c637d619ca39"
 
   def install
     bin.install "md2src"

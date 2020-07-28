cf88f91e560fb50e756ce2ad918df8c106b126a6
Alexander Willner <alex@willner.ws>
2020-07-28 19:04:25 +0200

    md2src 1.0.2

--- Formula/md2src.rb
+++ Formula/md2src.rb
@@ -1,9 +1,9 @@
 class Md2src < Formula
-  version "1.0.1"
+  version "1.0.2"
   desc "Markdown to source. Extracts code blocks marked with triple backticks into files."
   homepage "https://github.com/AlexanderWillner/md2src"
   url "https://github.com/AlexanderWillner/md2src/releases/download/v#{version}/md2src-#{version}-mac.tar.gz"
-  sha256 "49b358a48d2f2aadf83c8ebd61b784e1fbba8f96ae1a6ae99a7037b130b1f68c"
+  sha256 "e150724522ecd6acf96602a21040840a748c2325ac629ab418bda35fe74bd71f"
 
   def install
     bin.install "md2src"
